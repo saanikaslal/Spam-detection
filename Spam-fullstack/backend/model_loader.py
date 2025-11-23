d02f87bac8b0ef0a8e9d68ff56db410c77df527e
saanikaslal <98318779+saanikaslal@users.noreply.github.com>
2025-11-23 12:50:31 +0530

    fast api running

--- Spam-fullstack/backend/model_loader.py
+++ Spam-fullstack/backend/model_loader.py
@@ -1,16 +1,35 @@
 from tensorflow.keras.models import load_model #type:ignore
 import tensorflow as tf
+import json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
 
+#max_len read form config
+with open("saved-model/config.json") as f:
+    config = json.load(f)
+    MAX_LEN = config.get("MAX_LEN", 20)   # fallback = 20
+
+#loading model
 model = load_model("saved-model/spam_model.keras")
 
-def predict_message(message, tokenizer=None, max_len=100):
-    if tokenizer:
-        seq = tokenizer.texts_to_sequences([message])
-        padded = tf.keras.preprocessing.sequence.pad_sequences(seq, maxlen=max_len)
-    else:
-        # Here you will later add real preprocessing
-        padded = [message]
+# loading tokenizer
 
+with open("saved-model/tokenizer.json") as f:
+    data = json.load(f)
+    tokenizer = tokenizer_from_json(data)
+
+
+#prediction function 
+
+def predict_message(message:str, max_len=100):
+    
+    #convert text to integer sequence
+    seq = tokenizer.texts_to_sequences([message])
+    #padded to MAX_LEN
+    padded = pad_sequences(seq, maxlen=MAX_LEN)
+
+
+    #predict
     prediction = model.predict(padded)[0][0]
     label = "spam" if prediction > 0.5 else "not_spam"
 
